a280713100f864acbfe08d7d3fd8ef0b9c355d82
Sarah Newton <sarahnewton@Sarahs-MacBook.lan>
2022-03-05 10:40:57 +0100

    Fix bug with handling request errors

--- src/app/components/beerslist/beerslist.component.ts
+++ src/app/components/beerslist/beerslist.component.ts
@@ -1,21 +1,20 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { debounceTime, distinctUntilChanged, filter, Observable } from 'rxjs';
-import { BeersService } from '../../services/beers/beers.service';
-import { Pagination, PunkApiBeer } from '../../services/beers/types';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { Component, Input, OnInit } from "@angular/core";
+import { debounceTime, distinctUntilChanged, filter, Observable } from "rxjs";
+import { BeersService } from "../../services/beers/beers.service";
+import { Pagination, PunkApiBeer } from "../../services/beers/types";
+import { ActivatedRoute, NavigationEnd, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-beerslist',
-  templateUrl: './beerslist.component.html',
-  styleUrls: ['./beerslist.component.scss'],
+  selector: "app-beerslist",
+  templateUrl: "./beerslist.component.html",
+  styleUrls: ["./beerslist.component.scss"],
 })
 export class BeerslistComponent implements OnInit {
-  private searchTerm: string = '';
+  private searchTerm: string = "";
   private currentPage: number = 1;
   private maxPerPage: number = 20;
 
   beers$: Observable<PunkApiBeer[]> | undefined;
-  title: string = 'Beers list';
   constructor(
     private beersService: BeersService,
     private route: ActivatedRoute,
@@ -33,30 +32,26 @@ export class BeerslistComponent implements OnInit {
     // Allow a route params change to trigger state change in the component
     this.router.routeReuseStrategy.shouldReuseRoute = () => false;
 
-    this.searchTerm = this.route.snapshot.queryParams['beer_name'] ?? '';
+    this.searchTerm = this.route.snapshot.queryParams["beer_name"] ?? "";
     this.currentPage =
-      this.route.snapshot.queryParams['page'] ?? this.currentPage;
+      this.route.snapshot.queryParams["page"] ?? this.currentPage;
     this.maxPerPage =
-      this.route.snapshot.queryParams['per_page'] ?? this.maxPerPage;
+      this.route.snapshot.queryParams["per_page"] ?? this.maxPerPage;
     // TODO: Possible solution to manually keeping trakc of request rate limits
     // By delaying the requests by 1 second the rate limit will never be reached however
     // Currently does not work as expected with the router events and another solution in the BeerService
     // will most likely be a better approach
-    this.router.events
-      .pipe(
-        filter((event) => event instanceof NavigationEnd),
-        distinctUntilChanged(),
-        debounceTime(1000)
-      )
-      .subscribe(() => this.getBeers());
+    // this.router.events
+    //   .pipe(
+    //     filter((event) => event instanceof NavigationEnd),
+    //     distinctUntilChanged(),
+    //     debounceTime(1000)
+    //   )
+    //   .subscribe(() => this.getBeers());
 
     // How the request currently works
     if (!this.beers$) {
-      this.beers$ = this.beersService.getBeers(
-        this.currentPage,
-        this.maxPerPage,
-        this.searchTerm
-      );
+      this.getBeers();
     }
   }
 }
--- src/app/services/beers/beers.service.ts
+++ src/app/services/beers/beers.service.ts
@@ -1,41 +1,46 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
-import { Observable, of, throwError } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
-import { PunkApiBeer } from './types';
+import { Observable, of, throwError } from "rxjs";
+import { catchError, map } from "rxjs/operators";
+import { PunkApiBeer } from "./types";
 
-@Injectable({ providedIn: 'root' })
+@Injectable({ providedIn: "root" })
 export class BeersService {
   // BaseUrl to PunkApi
-  private punkApiUrl = 'https://api.punkapi.com/v2/beers';
+  private punkApiUrl = "https://api.punkapi.com/v2/beers";
 
   httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+    headers: new HttpHeaders({ "Content-Type": "application/json" }),
   };
 
   handleRateLimit = (remaining: string | null) => {
     localStorage.setItem(
-      'PunkApi_RateLimitRemaining',
-      remaining?.toString() ?? ''
+      "PunkApi_RateLimitRemaining",
+      remaining?.toString() ?? ""
     );
+    // TODO: This is not really handled right now and just throws an error.
+    // Ideally we would either show a message to the user or create some kind of retry system
+    if (Number(remaining) == 0) {
+      throw throwError(() => new Error("Rate limit reached"));
+    }
   };
 
   getRateLimit = (): number => {
-    var remaining = localStorage.getItem('PunkApi_RateLimitRemaining');
+    var remaining = localStorage.getItem("PunkApi_RateLimitRemaining");
     // If rate limit not stored, assume 1
-    return Number(remaining) ?? 1;
+    return isNaN(Number(remaining)) ? 1 : Number(remaining);
   };
 
   constructor(private http: HttpClient) {}
 
   getPreviousSearches = (term: string): string[] => {
-    var savedSearchTerms = localStorage.getItem('PunkApi_SavedSearches');
+    var savedSearchTerms = localStorage.getItem("PunkApi_SavedSearches");
     if (!savedSearchTerms?.length) return [];
     // Trim any trailing whitespace
     term.trim();
     var result: string[] =
-      term != ''
+      term != ""
         ? (JSON.parse(savedSearchTerms) as string[])?.filter((x) =>
             x.toLowerCase().includes(term.toLowerCase())
           )
@@ -44,7 +49,7 @@ export class BeersService {
   };
 
   setNewSearches = (term: string): void => {
-    var savedSearchTerms = localStorage.getItem('PunkApi_SavedSearches');
+    var savedSearchTerms = localStorage.getItem("PunkApi_SavedSearches");
     var newSearchTerms: string[] = [];
 
     newSearchTerms = savedSearchTerms?.length
@@ -57,7 +62,7 @@ export class BeersService {
     newSearchTerms.push(term);
 
     localStorage.setItem(
-      'PunkApi_SavedSearches',
+      "PunkApi_SavedSearches",
       JSON.stringify(newSearchTerms)
     );
   };
@@ -68,32 +73,24 @@ export class BeersService {
     perPage: number,
     searchTerm: string
   ): Observable<PunkApiBeer[]> => {
-    var remaining = this.getRateLimit();
-    if (remaining == 0) {
-      // TODO: This is not really handled right now and just prevents
-      // the request from firing. Ideally we would either show a message to the user
-      // or create some kind of retry system
-      throw throwError(() => new Error('Rate limit reached'));
-    } else {
-      return this.http
-        .get<PunkApiBeer[]>(
-          `${this.punkApiUrl}?page=${page}&per_page=${perPage}${
-            searchTerm && `&beer_name=${searchTerm}`
-          }`,
-          { observe: 'response' }
-        )
-        .pipe(
-          map((response: any) => {
-            this.handleRateLimit(response.headers.get('x-ratelimit-remaining'));
-            return response.body;
-          }),
-          catchError(this.handleError<any>('getBeers', []))
-        );
-    }
+    return this.http
+      .get<PunkApiBeer[]>(
+        `${this.punkApiUrl}?page=${page}&per_page=${perPage}${
+          searchTerm && `&beer_name=${searchTerm}`
+        }`,
+        { observe: "response" }
+      )
+      .pipe(
+        map((response: any) => {
+          this.handleRateLimit(response.headers.get("x-ratelimit-remaining"));
+          return response.body;
+        }),
+        catchError(this.handleError<any>("getBeers", []))
+      );
   };
 
   // Handle Http operation that failed.
-  private handleError<T>(operation = 'operation', result?: T) {
+  private handleError<T>(operation = "operation", result?: T) {
     return (error: any): Observable<T> => {
       console.error(error); // log to console
       // Let the app keep running by returning an empty result.
